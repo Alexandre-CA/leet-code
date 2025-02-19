b6335cdd26481876d959af0ed8481f55a891c1c7
alexandre <alexandre220404@gmail.com>
2025-02-18 21:37:28 -0300

    :recycle: refactor: Explanation `1 Two Sum`

--- 1-two-sum.js
+++ 1-two-sum.js
@@ -10,12 +10,22 @@ You can return the answer in any order.
 
  * */
 
+/**
+ *! Explanation:
+  * 1. Create a variable to store the values already seen.
+  * 2. Loop through the nums array.
+  * 3. Create a variable to store the target to goes. ( To verify if the target_to_goes is in the values_prev array).
+  * 4. If the target_to_goes is in the values_prev array, return the index of the target_to_goes and the current index.
+  * 5. Else add the current value to the values_prev array.
+  * 6. Return the values_prev array.
+ */
+
 function twoSum(nums, target) {
   let values_prev = [];
 
   for (let index = 0; index < nums.length; index++) {
     const target_to_goes = target - nums[index];
-    if(values_prev.includes(target_to_goes)){
+    if (values_prev.includes(target_to_goes)) {
       return [values_prev.indexOf(target_to_goes), index];
     }
     values_prev.push(nums[index]);
